7456fa4ee735d823ebd7490594bee79669a1089a
Tobita-Haruhiko <period9801@gmail.com>
2024-10-08 08:49:12 +0900

    正誤判定　

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect, useRef, useState } from "react";
 
 function App() {
+  let ctx = new AudioContext()
+
+  const sleep = (duration: number) => new Promise(resolve => setTimeout(resolve, duration))
+
+  const [answer, setAnswer] = useState<string>("")
+  const [correct, setCorrect] = useState<string>("")
+  const answerRef = useRef(answer);
+  const correctRef = useRef(correct);
+  useEffect(() => {
+    answerRef.current = answer
+  }, [answer])
+  useEffect(() => {
+    correctRef.current = correct
+  }, [correct])
+
+  const oscillator = async(freq: number) => {
+    await ctx.resume();
+    let osc = new OscillatorNode(ctx)
+    osc.frequency.value = freq
+    osc.type = "sine"
+    osc.connect(ctx.destination)
+    osc.start();
+    await sleep(1000);
+    osc.stop();
+  };
+
+  const looping = async() => {
+    for (let i = 0; i < 5; i++) {
+      await oscillator(440);
+      await sleep(1000);
+
+      var rand = Math.random();
+      if (rand < 0.5) {
+        setCorrect("High");
+        await oscillator(445);
+        
+      } else {
+        setCorrect("Low");
+        await oscillator(435);
+        
+      }
+      await sleep(3000);
+
+      console.log(answerRef.current)
+      console.log(correctRef.current)
+
+      if (answerRef.current === correctRef.current) {
+        console.log("correct")
+      } else {
+        console.log("wrong")
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={looping}>start</button>
+    <button onClick={() => setAnswer("High")}>high</button>
+    <button onClick={() => setAnswer("Low")}>low</button>
+    <button onClick={() => console.log(answer)}>ans?</button>
+    <p>Answer: {answer}</p>
+    <p>Correct: {correct}</p>
+    </>
   );
 }
 
